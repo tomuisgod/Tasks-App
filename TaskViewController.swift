b2d6b28f27c9aa032f2790d37af81fbab35bdf5c
tomu <66744640+tomuisgod@users.noreply.github.com>
2020-12-19 17:45:51 +0100

    Setup

---
+++ TaskViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TaskViewController.swift
+//  Tasks
+//
+//  Created by Tomáš Lovrant on 19/12/2020.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
