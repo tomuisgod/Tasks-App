420c9c90abe8ac39fddf4d45eb2f384b2d20aabe
tomu <66744640+tomuisgod@users.noreply.github.com>
2020-12-19 18:00:03 +0100

    Update TaskViewController.swift

--- TaskViewController.swift
+++ TaskViewController.swift
@@ -2,17 +2,28 @@
 //  TaskViewController.swift
 //  Tasks
 //
-//  Created by Tomáš Lovrant on 19/12/2020.
+//  Created by tomu on 19/12/2020.
 //
 
 import UIKit
 
 class TaskViewController: UIViewController {
-
+    @IBOutlet var label: UILabel!
+    var task: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        label.text = task
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Delete", style: .done, target: self, action: #selector(deleteTask))
     }
     
+    @objc func deleteTask() {
+        
+        let newCount = count - 1;
+        
+        UserDefaults().setValue(newCount, forKey: "count")
+        UserDefaults().setValue(nil, forKey: "task_\(currentPosition)")
+
+    }
 }
